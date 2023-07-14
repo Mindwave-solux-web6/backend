b40668e7a0e4c0d2a65f715133e5e12418d0ac60
jungeun <sm2110147wje@sookmyung.ac.kr>
2023-07-14 14:33:29 +0900

    다이어리

---
+++ src/main/java/com/wave/diary/controller/PostsApiController.java
@@ -0,0 +1,12 @@
+package com.wave.diary.controller;
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/wave/diary/repository/PostsRepository.java
@@ -0,0 +1,5 @@
+package com.wave.diary.repository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}

47825c8dbe61cf5e9a022792e524c6c81df0d383
jungeun <sm2110147wje@sookmyung.ac.kr>
2023-07-19 15:59:07 +0900

    [del]#2-diary

--- src/main/java/com/wave/diary/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wave.diary.repository;
-
-import com.wave.diary.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}

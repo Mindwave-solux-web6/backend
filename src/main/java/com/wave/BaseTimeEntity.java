20592fa4f089d1ba49e523dd5197e1bf3ef21229
isuHan <116141262+isuHan@users.noreply.github.com>
2023-07-26 11:13:24 +0900

    다이어리

--- src/main/java/com/wave/BaseTimeEntity.java
+++ src/main/java/com/wave/BaseTimeEntity.java
@@ -5,9 +5,11 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import javax.persistence.Column;
 
 @Getter
 @MappedSuperclass
@@ -15,6 +17,7 @@ import java.time.LocalDateTime;
 //모든 Entity 의 상위 클래스에서 createdDate, updatedDate 를 자동으로 관리해주는 역할
 public class BaseTimeEntity {
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate; //생성시 날짜 자동 생성
 
     @LastModifiedDate
---
+++ src/main/java/com/wave/diary/controller/BoardController.java
@@ -0,0 +1,58 @@
+package com.wave.diary.controller;
+
+import com.wave.diary.dto.BoardDto;
+import com.wave.diary.service.BoardService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/diary")
+public class BoardController {
+    private BoardService boardService;
+
+
+    /* 일기 쓰기 */
+    @PostMapping("/post")
+    public Long savePost(@RequestBody BoardDto boardDto) {
+        return boardService.savePost(boardDto);
+    }
+
+
+    /* 일기 수정 */
+    @PutMapping("/post/{id}")
+    public void updatePost(@PathVariable("id") Long id, @RequestBody BoardDto boardDto) {
+        boardDto.setId(id);
+        boardService.savePost(boardDto);
+    }
+
+    /* 일기 목록 */
+    @GetMapping("/list")
+    public List<BoardDto> getBoardList(@RequestParam(value = "page", defaultValue = "1") Integer pageNum) {
+        return boardService.getBoardlist(pageNum);
+    }
+
+
+    /* 일기 상세 */
+    @GetMapping("/post/{id}")
+    public BoardDto getPost(@PathVariable("id") Long id) {
+        return boardService.getPost(id);
+    }
+
+
+    /*일기 검색*/
+    @GetMapping("/search")
+    public List<BoardDto> searchPosts(@RequestParam(value = "keyword") String keyword) {
+        return boardService.searchPosts(keyword);
+    }
+
+
+    /* 일기 삭제 */
+    @DeleteMapping("/post/{id}")
+    public void deletePost(@PathVariable("id") Long id) {
+        boardService.deletePost(id);
+    }
+
+}

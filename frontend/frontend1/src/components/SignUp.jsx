5bc5a340dc456f080243cdf9ae389fd53a9b3233
zeeun <98653814+zeeun@users.noreply.github.com>
2023-07-29 14:13:28 +0900

    [FEAT]SIGNUP

--- frontend/frontend1/src/components/SignUp.jsx
+++ frontend/frontend1/src/components/SignUp.jsx
@@ -78,11 +78,10 @@ const SignUp = () => {
         }),
       });
 
-      // Redirect to the desired route or provide appropriate feedback after successful submission
     }
   };
   return (
-    <header className="masthead">
+<header className="masthead">
       <section id="signup" className="mt-7">
         <hr className="sign-divider-light" />
         <h2 className="text-center mt-0 text-white">Sign Up</h2>
@@ -91,14 +90,17 @@ const SignUp = () => {
         <div className="row gx-4 gx-lg-5 justify-content-center mb-5">
           <div className="col-lg-4">
             <form id="signup" onSubmit={submitSignUp}>
-              {/* ID input */}
+              {/* NAME input */}
               <div className="form-floating mb-3">
                 <input
                   className="form-control"
                   id="name"
+                  name="name" // name 속성을 추가해야 서버에서 구분할 수 있습니다.
                   type="text"
                   placeholder="Enter your name..."
                   required
+                  value={name} // 값을 state 변수로 설정해줍니다.
+                  onChange={handleNameChange} // name 값이 변경될 때마다 state 변수를 업데이트합니다.
                 />
                 <label htmlFor="name">Full name</label>
                 <div
@@ -114,9 +116,12 @@ const SignUp = () => {
                 <input
                   className="form-control"
                   id="email"
+                  name="email" // name 속성을 추가해야 서버에서 구분할 수 있습니다.
                   type="email"
                   placeholder="Enter your Email..."
                   required
+                  value={email} // 값을 state 변수로 설정해줍니다.
+                  onChange={handleEmailChange} // name 값이 변경될 때마다 state 변수를 업데이트합니다.
                 />
                 <label htmlFor="email">Email address</label>
                 <div
@@ -133,16 +138,19 @@ const SignUp = () => {
                 </div>
               </div>
 
-              {/* ID input */}
+              {/* NICKNAME input */}
               <div className="form-floating mb-3">
                 <input
                   className="form-control"
-                  id="id"
+                  id="nickname"
+                  name="nickname" // name 속성을 추가해야 서버에서 구분할 수 있습니다.
                   type="text"
                   placeholder="Enter your ID..."
                   required
+                  value={nickname} // 값을 state 변수로 설정해줍니다.
+                  onChange={handleNicknameChange} // name 값이 변경될 때마다 state 변수를 업데이트합니다.
                 />
-                <label htmlFor="id">ID</label>
+                <label htmlFor="nickname">NICKNAME</label>
                 <div
                   className="invalid-feedback"
                   data-sb-feedback="id:required"
@@ -156,6 +164,7 @@ const SignUp = () => {
                 <input
                   className="form-control"
                   id="password"
+                  name="password" // name 속성을 추가해야 서버에서 구분할 수 있습니다.
                   type="password"
                   placeholder="Enter your password..."
                   value={password}
@@ -176,6 +185,7 @@ const SignUp = () => {
                 <input
                   className="form-control"
                   id="confirmPassword"
+                  name="confirmPassword" // name 속성을 추가해야 서버에서 구분할 수 있습니다.
                   type="password"
                   placeholder="Enter your password again..."
                   value={confirmPassword}
@@ -223,6 +233,148 @@ const SignUp = () => {
         </div>
       </section>
     </header>
+
+    // <header className="masthead">
+    //   <section id="signup" className="mt-7">
+    //     <hr className="sign-divider-light" />
+    //     <h2 className="text-center mt-0 text-white">Sign Up</h2>
+    //     <hr className="sign-divider-light mb-5" />
+
+    //     <div className="row gx-4 gx-lg-5 justify-content-center mb-5">
+    //       <div className="col-lg-4">
+    //         <form id="signup" onSubmit={submitSignUp}>
+    //           {/* ID input */}
+    //           <div className="form-floating mb-3">
+    //             <input
+    //               className="form-control"
+    //               id="name"
+    //               type="text"
+    //               placeholder="Enter your name..."
+    //               required
+    //             />
+    //             <label htmlFor="name">Full name</label>
+    //             <div
+    //               className="invalid-feedback"
+    //               data-sb-feedback="name:required"
+    //             >
+    //               A name is required.
+    //             </div>
+    //           </div>
+
+    //           {/* EMAIL input */}
+    //           <div className="form-floating mb-3">
+    //             <input
+    //               className="form-control"
+    //               id="email"
+    //               type="email"
+    //               placeholder="Enter your Email..."
+    //               required
+    //             />
+    //             <label htmlFor="email">Email address</label>
+    //             <div
+    //               className="invalid-feedback"
+    //               data-sb-feedback="email:required"
+    //             >
+    //               An email is required.
+    //             </div>
+    //             <div
+    //               className="invalid-feedback"
+    //               data-sb-feedback="email:email"
+    //             >
+    //               Email is not valid.
+    //             </div>
+    //           </div>
+
+    //           {/* ID input */}
+    //           <div className="form-floating mb-3">
+    //             <input
+    //               className="form-control"
+    //               id="id"
+    //               type="text"
+    //               placeholder="Enter your ID..."
+    //               required
+    //             />
+    //             <label htmlFor="id">ID</label>
+    //             <div
+    //               className="invalid-feedback"
+    //               data-sb-feedback="id:required"
+    //             >
+    //               An ID is required.
+    //             </div>
+    //           </div>
+
+    //           {/* PASSWORD input */}
+    //           <div className="form-floating mb-3">
+    //             <input
+    //               className="form-control"
+    //               id="password"
+    //               type="password"
+    //               placeholder="Enter your password..."
+    //               value={password}
+    //               onChange={handlePasswordChange}
+    //               required
+    //             />
+    //             <label htmlFor="password">Password</label>
+    //             <div
+    //               className="invalid-feedback"
+    //               data-sb-feedback="password:required"
+    //             >
+    //               A password is required.
+    //             </div>
+    //           </div>
+
+    //           {/* PASSWORD CONFIRM input */}
+    //           <div className="form-floating mb-3">
+    //             <input
+    //               className="form-control"
+    //               id="confirmPassword"
+    //               type="password"
+    //               placeholder="Enter your password again..."
+    //               value={confirmPassword}
+    //               onChange={handleConfirmPasswordChange}
+    //               required
+    //             />
+    //             <label htmlFor="confirmPassword">Password Check</label>
+    //             <div
+    //               className="invalid-feedback"
+    //               data-sb-feedback="confirmPassword:required"
+    //             >
+    //               A password is required.
+    //             </div>
+    //           </div>
+
+    //           {!passwordsMatch && (
+    //             <p className="text-white text-center">
+    //               비밀번호와 비밀번호 확인이 일치하지 않습니다.
+    //             </p>
+    //           )}
+
+    //           {/* SUBMIT button */}
+    //           <div className="d-grid">
+    //             <button
+    //               className="btn btn-primary btn-xl"
+    //               id="submitButton"
+    //               type="submit"
+    //             >
+    //               Sign Up !
+    //             </button>
+    //           </div>
+    //         </form>{" "}
+    //         <br />
+    //         <div className="text-center text-white">
+    //           이미 계정이 있다면&nbsp;
+    //           <Link className="text-center text-white" to="/signin">
+    //             <i
+    //               className="fa-solid fa-right-to-bracket"
+    //               style={{ color: "#ffffff" }}
+    //             ></i>
+    //             &nbsp;로그인하기
+    //           </Link>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </section>
+    // </header>
   );
 };
 

6b71f9bea7a16c2f82d5d78e1767fe5eb0f0b63a
isuHan <116141262+isuHan@users.noreply.github.com>
2023-08-01 07:45:23 +0900

    api 수정

--- src/main/java/com/wave/DiaryAndLetterController.java
+++ src/main/java/com/wave/DiaryAndLetterController.java
@@ -1,11 +1,14 @@
 package com.wave;
 
 import com.wave.diary.domain.BoardEntity;
+import com.wave.diary.dto.BoardDto;
 import com.wave.diary.service.BoardService;
 import com.wave.letter.domain.LetterEntity;
+import com.wave.letter.dto.LetterDto;
 import com.wave.letter.service.LetterService;
 import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,4 +39,29 @@ public class DiaryAndLetterController {
 
         return ResponseEntity.ok(response);
     }
+
+    @GetMapping("/diary")
+    public ResponseEntity<BoardDto> getDiaryByDate(@RequestParam("date")
+                                                   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        BoardEntity diary = boardService.getBoardByDate(date);
+
+        if (diary == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(boardService.convertToDto(diary));
+    }
+
+    @GetMapping("/letter")
+    public ResponseEntity<LetterDto> getLetterByDate(@RequestParam("date")
+                                                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        LetterEntity letter = letterService.getLetterByDate(date);
+
+        if (letter == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(letterService.convertToDto(letter));
+    }
+
 }
